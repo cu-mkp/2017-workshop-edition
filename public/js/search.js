fc197e40b3b70e80984c897202f21f71b45155c1
ludotzp <ludovic.touzepeiffer@gmail.com>
2017-07-19 13:44:25 -0400

    Update search.js

--- public/js/search.js
+++ public/js/search.js
@@ -11,6 +11,7 @@ var index = lunr(function () {
   this.field('layout')
   this.field('content')
   this.ref('id')
+  this.field('mode')
 });
 
 //Add to this index the proper metadata from the Jekyll content
@@ -21,24 +22,27 @@ index.add({
   title: {{text.title | jsonify}},
   author: {{text.author | jsonify}},
   content: {{text.content | jsonify | strip_html}},
+  mode: {{text.mode | jsonify}},        
   id: {{count}}
 });{% assign count = count | plus: 1 %}{% endfor %}
 console.log( jQuery.type(index) );
 
-// Builds reference data (maybe not necessary for us, to check)
 
+
+// Builds reference data (maybe not necessary for us, to check)
 
 var store = [{% for text in site.texts %}{
   "title": {{text.title | jsonify}},
   "author": {{text.author | jsonify}},
   "layout": {{ text.layout | jsonify }},
   "link": {{text.url | jsonify}},
-  "excerpt": {{text.content | strip_html |remove: "-"| remove: "[TOC] | "| replace: '[diplomatic]', '<b>Translation</b>'| replace: '[translation]', '<b>Diplomatic</b>'| truncatewords: 20 | jsonify}}
+  "mode": {{text.mode | jsonify}}, 
+  "excerpt": {{text.mode | append: " " | append: text.content | strip_html | remove: "[translation]" | remove: "[diplomatic]" |remove: "-"| remove: "[TOC] | "| replace: 'tcn', '<b>Normalized Transcription</b>'|replace: 'tc', '<b>Transcription</b>'| replace: 'tl', '<b>Translation</b>'| truncatewords: 20 | jsonify}}
 }
 {% unless forloop.last %},{% endunless %}{% endfor %}]
 
 //Query
-
+                                        
 var qd = {}; //Gets values from the URL
 location.search.substr(1).split("&").forEach(function(item) {
     var s = item.split("="),
@@ -46,7 +50,7 @@ location.search.substr(1).split("&").forEach(function(item) {
         v = s[1] && decodeURIComponent(s[1]);
     (k in qd) ? qd[k].push(v) : qd[k] = [v]
 });
-
+                                        
 function doSearch() {
   var resultdiv = $('#results');
   var query = $('input#search').val();
@@ -57,18 +61,38 @@ function doSearch() {
   resultdiv.empty();
   if (result.length == 0) {
     resultdiv.append('<p class="">No results found.</p>');
-  } else if (result.length == 1) {
-    resultdiv.append('<p class="">Found '+result.length+' result</p>');
-  } else {
-    resultdiv.append('<p class="">Found '+result.length+' results</p>');
-  }
+  } 
   //Loop through, match, and add results
   for (var item in result) {
     var ref = result[item].ref;
-    var searchitem = '<div class="result"><a href="{{ site.baseurl }}'+store[ref].link+'?q='+query+'">'+store[ref].title+'</a>';
-    var end = '<p>'+store[ref].excerpt+'</p></div>';
-    searchitem += end;
-    resultdiv.append(searchitem);
+        if($('input[name=tc]').is(':checked') && store[ref].mode == "tc"){
+              var searchitem = '<div class="result"><a href="{{ site.baseurl }}'+store[ref].link+'?q='+query+'">'+store[ref].title+'</a>';
+              var end = '<p>'+store[ref].excerpt+'</p></div>';
+              searchitem += end;
+              resultdiv.append(searchitem);
+        }
+    
+      if($('input[name=tcn]').is(':checked') && store[ref].mode == "tcn"){
+                var searchitem = '<div class="result"><a href="{{ site.baseurl }}'+store[ref].link+'?q='+query+'">'+store[ref].title+'</a>';
+                var end = '<p>'+store[ref].excerpt+'</p></div>';
+                searchitem += end;
+                resultdiv.append(searchitem);
+          }
+
+      if($('input[name=tl]').is(':checked') && store[ref].mode == "tl"){
+                var searchitem = '<div class="result"><a href="{{ site.baseurl }}'+store[ref].link+'?q='+query+'">'+store[ref].title+'</a>';
+                var end = '<p>'+store[ref].excerpt+'</p></div>';
+                searchitem += end;
+                resultdiv.append(searchitem);
+          }
+    
+      if(!$('input[name=tc]').is(':checked') && !$('input[name=tcn]').is(':checked') && !$('input[name=tl]').is(':checked')){
+           var searchitem = '<div class="result"><a href="{{ site.baseurl }}'+store[ref].link+'?q='+query+'">'+store[ref].title+'</a>';
+                var end = '<p>'+store[ref].excerpt+'</p></div>';
+                searchitem += end;
+                resultdiv.append(searchitem);
+      }
+  
    }
   }
 
